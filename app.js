5555627d285c201c04eb399fcc8acf4d79d7d1c9
Neeraj Doharey <neeraj.d@39shops.com>
2017-02-02 21:26:14 +0530

    Initial template

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.use('/',express.static('public'));
+
+app.listen("3000",function(){ 
+	console.log("Server Started");
+});
+
+app.set('view engine','pug');
+
+// app.get('/',function(req,res){
+// 	res.render('index')
+// })
